5ff2092b28ecf50ed9b07e893e42bf07169e8a8d
kvaishak <kvaishak47@gmail.com>
2023-01-25 11:38:43 +0530

    Create function for button click + removed the hard coding

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var playerCard = "card7"
+    var cpuCard = "card13"
+    
+    var playerScore = 0
+    var cpuScore = 0
+    
     var body: some View {
         ZStack{
            
@@ -21,13 +28,21 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Image("button")
+
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
+               
+                
                 Spacer()
                 HStack{
                     Spacer()
@@ -35,7 +50,7 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -43,7 +58,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }
                     Spacer()
@@ -52,6 +67,10 @@ struct ContentView: View {
                 Spacer()
             }
         }
+    }
+    
+    func deal(){
+        print("Deal Cards")
     }
 }
 
